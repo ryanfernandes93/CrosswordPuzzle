e1cd6cb2ad13f6ecf371d9a78a853529c10ee370
Ryan <Ryan@192.168.2.16>
2019-10-21 18:43:35 -0300

    Final Code

--- Crossword/src/CrosswordMain.java
+++ Crossword/src/CrosswordMain.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class CrosswordMain {
 
@@ -9,10 +10,12 @@ public class CrosswordMain {
 		// TODO Auto-generated method stub
 		BufferedReader stream=new BufferedReader(new InputStreamReader(System.in));
 		FillinPuzzle fp=new FillinPuzzle();
-		fp.loadPuzzle(stream);
-		fp.print();
-		fp.solve();
-		fp.print();
+		System.out.println("load puzzle "+fp.loadPuzzle(stream));
+		//fp.print();
+		System.out.println("solve puzzle "+fp.solve());
+		PrintWriter outstream=new PrintWriter(System.out);
+		fp.print(outstream);
+		System.out.println("number of choices "+fp.choices());
 	}
 
 }
